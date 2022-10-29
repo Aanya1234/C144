fb33a6cbb171ad8a6f84c80297aca70f879c6bc9
Aanya1234 <85971907+Aanya1234@users.noreply.github.com>
2022-10-29 11:45:39 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,13 @@
+a = 33
+b = "33"
+if b > a:
+    print("b is greather than a")
+elif a == b:
+    print("a and b are equal")
+
+def my_function(Fname):
+    print( fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
